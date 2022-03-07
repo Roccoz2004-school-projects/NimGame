1da1d6cec24f1b3590df0dc0884b6169464ea368
ComputerLab <roccoz2004@gmail.com>
2022-03-07 08:42:02 -0800

    WIP - Adding player and computer turns

--- src/com/example/NimGameStarters.java
+++ src/com/example/NimGameStarters.java
@@ -6,11 +6,15 @@ public class NimGameStarters {
     public static int makeMarkers() {
         return ((int) (Math.random() * 25 + 20));
     }
+    public static int numMarkers = makeMarkers();
 
     public static boolean computerMoveFirst(int num) {
         boolean firstMove = false;
         if (num % 4 != 1) {
             firstMove = true;
+
+        } else {
+
         }
         return firstMove;
     }
@@ -26,9 +30,23 @@ public class NimGameStarters {
 
     public static void main(String[] args) {
         String player = userNamePrompt();
-        int numMarkers = makeMarkers();
+
         System.out.println("This game will be played with " + numMarkers + " markers.");
         System.out.println(computerMoveFirst(numMarkers));
+        starterMove();
+    }
+
+    public static int broadcastMarkers() {
+        numMarkers = numMarkers - 1;
+        return numMarkers;
+    }
+    public static void starterMove() {
+        if (computerMoveFirst(numMarkers) == true) {
+            System.out.println("Comp. Move");
+
+        } else {
+            System.out.println("Player Move");
+        }
 
     }
 }
