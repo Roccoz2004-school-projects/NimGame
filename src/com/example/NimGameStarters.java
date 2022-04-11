eaaa4df1009e4c1d794a4d5a123aa28599f6b6a2
Roccoz2004 <roccoz2004@gmail.com>
2022-04-11 04:05:45 -0700

    Completed Game of Nim Balanced difficulty (winnable)

--- src/com/example/NimGameStarters.java
+++ src/com/example/NimGameStarters.java
@@ -7,7 +7,8 @@ public class NimGameStarters {
         return ((int) (Math.random() * 25 + 20));
     }
     public static int numMarkers = makeMarkers(); //Creates random amount of markers
-    public static int currentTurn = 0;
+    public static int currentTurn = 0; //Initialize turn counter
+    public static String playerName = ""; //Initialize player's name
 
     public static boolean queryFirstMove(int num) {
         boolean firstMove = false;
@@ -19,46 +20,25 @@ public class NimGameStarters {
 
     public static String userNamePrompt() {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please type in your name. ");
-        String playerName = input.nextLine();
-        System.out.println("Welcome to the game of Nim, " + playerName);
-        System.out.println();
+        System.out.print("Please type in your name... ");
+        playerName = input.nextLine();
+        System.out.println("Welcome to the game of Nim, " + playerName + "!\n");
         return playerName;
     } //Gets Player's name from input, and outputs a welcome msg.
 
     public static void main(String[] args) {
         String player = userNamePrompt();
-
-        System.out.println("This game will be played with " + numMarkers + " markers.");
+        System.out.println("This game will be played with " + numMarkers + " markers.\n");
         starter();
     }
 
-    public static int broadcastMarkers() {
-        numMarkers = numMarkers - 1;
-        return numMarkers;
-    }
-
     public static void starter() {
-        System.out.println(currentTurn);
         if (queryFirstMove(numMarkers) == true) {
-            System.out.println("-- Comp. Move --"); //Delete later
             currentTurn = 0;
             Turns.routine();
         } else {
-            System.out.println("-- Player Move --"); //Delete later
             currentTurn = 1;
             Turns.routine();
         }
     } //Sets first move to either Computer or Player based on query, then sends next move to routine
-
-
-
-    public static void endGame() {
-        if (numMarkers == 1) {
-            System.out.println("Game Over!");
-            if (currentTurn == 0) {
-                System.out.println("The Computer wins!");
-            }
-        }
-    }
 }
--- src/com/example/Turns.java
+++ src/com/example/Turns.java
@@ -3,30 +3,66 @@ package com.example;
 import java.util.Scanner;
 
 public class Turns {
+
+    public static NimGameStarters Nim = new NimGameStarters();
+    static int markers = Nim.numMarkers;
+
     public static void routine() {
-        System.out.println("--Routine--");
-        if (NimGameStarters.currentTurn == 0) {
-            compTurn();
-        } else if (NimGameStarters.currentTurn == 1) {
-            playerTurn();
+        if (Nim.currentTurn == 0) {
+            markers = (markers - compTurn());
+            Nim.currentTurn = 1;
+        } else if (Nim.currentTurn == 1) {
+            markers = (markers - playerTurn());
+            Nim.currentTurn = 0;
         }
+        System.out.println("There are " + markers + " markers left.\n");
+        queryEndGame();
+        routine();
     }
 
+    /*
+        Player enters # of markers to take (1, 2, or 3)
+        Check for valid input
+        If input is NOT valid: show error, repeat prompt
+            Else (input is valid): return # of markers taken
+    */
     public static int playerTurn() {
+        System.out.println(Nim.playerName + "'s Turn:");
         Scanner input = new Scanner(System.in);
-        System.out.print("Please take 1, 2, or 3 markers. ");
-        int playerTake = Integer.parseInt(input.nextLine());
+        System.out.print("Please take 1, 2, or 3 markers... ");
+        int playerTake = Integer.parseInt(input.nextLine()); //Ask for player input
         if ((playerTake == 1)||(playerTake == 2)||(playerTake == 3)) {
             System.out.println("You picked " + playerTake + " markers.");
         } else {
-            System.out.println("Please take ONLY 1, 2, or 3 markers!");
+            System.out.println("Please take ONLY 1, 2, or 3 markers!"); //If take is not valid, show error
             playerTurn();
         }
         return playerTake;
     }
 
     public static int compTurn() {
-        System.out.println("COMPUTER TURN");
-        return
+        int num = 0;
+        System.out.println("Computer's Turn:");
+        if ((markers - 1) % 3 == 0) {
+            num = 1;
+        } else if ((markers - 2) % 3 == 0) {
+            num = 2;
+        } else if ((markers - 3) % 3 == 0) {
+            num = 3;
+        }
+        System.out.println("Computer picked " + num + " markers.");
+        return num;
+    }
+
+    public static void queryEndGame() {
+        if (markers == 0) {
+            System.out.println("Game Over!");
+            if (Nim.currentTurn == 0) {
+                System.out.println("The Computer wins!");
+            } else if (Nim.currentTurn == 1) {
+                System.out.println("The Player wins!");
+            }
+            System.exit(0);
+        }
     }
 }
